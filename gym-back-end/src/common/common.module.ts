327b3f58a7288a8981cdc4b7d651e87423e5987c
Franco_Serjen <francoserjen100@gmail.com>
2024-09-17 11:08:54 -0300

    Instalación de typeORM y refactorización del codigo para su implementación

--- gym-back-end/src/common/common.module.ts
+++ gym-back-end/src/common/common.module.ts
@@ -6,6 +6,8 @@ import { JwtModule } from '@nestjs/jwt';
 import { LoginService } from './services/login.service';
 import { LoginController } from './controllers/login.controller';
 import * as dotenv from 'dotenv';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
 
 const result = dotenv.config();
 
@@ -15,6 +17,18 @@ if (result.error) {
 
 @Module({
     imports: [
+        TypeOrmModule.forRoot({
+            type: 'mysql',
+            host: process.env.DB_HOST,
+            port: Number(process.env.DB_PORT),
+            username: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_DATABASE,
+            synchronize: false,
+            entities: ['dist/**/*.entity.js'],
+            // logging: 'all',
+        }),
+        TypeOrmModule.forFeature([User]),
         JwtModule.register({
             secret: process.env.JWT_SECRET_KEY,
             signOptions: { expiresIn: '60s' },
