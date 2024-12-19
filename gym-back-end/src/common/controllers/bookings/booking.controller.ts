2ebb46e092a6a303bda70d4829de511e9d9e0f22
Franco_Serjen <francoserjen100@gmail.com>
2024-12-19 16:32:14 -0300

    Delete de booking por id terminado

--- gym-back-end/src/common/controllers/bookings/booking.controller.ts
+++ gym-back-end/src/common/controllers/bookings/booking.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Post } from '@nestjs/common';
 import { IBooking } from 'src/common/models/interfaces/booking.interface';
 import { BookingService } from 'src/common/services/bookings/booking.service';
 
@@ -11,6 +11,15 @@ export class BookingController {
     async createBooking(@Body('appointmentId') appointmentId: number, @Body('userId') userId: number): Promise<IBooking> {
         try {
             return await this.bookingServices.createBooking(appointmentId, userId);
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    @Delete()
+    async deleteBooking(@Body('bookingId') bookingId: number) {
+        try {
+            return await this.bookingServices.deleteBooking(bookingId);
         } catch (error) {
             throw error;
         }
