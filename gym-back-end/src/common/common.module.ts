b997172edf118fc316b2761175b2345705a92cb9
Franco_Serjen <francoserjen100@gmail.com>
2025-05-04 22:16:16 -0300

    Creacion de guard de verificacion de token para proteger rutas de clientes y administradores

--- gym-back-end/src/common/common.module.ts
+++ gym-back-end/src/common/common.module.ts
@@ -1,14 +1,13 @@
 import { Module } from '@nestjs/common';
-import { JwtModule } from '@nestjs/jwt';
 import * as dotenv from 'dotenv';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './models/entities/user.entity';
 import { Appointment } from './models/entities/appointment.entity';
 import { Booking } from './models/entities/booking.entity';
 import { AppointmentService } from './services/appointments/appointment.service';
 import { AppointmentController } from './controllers/appointments/appointment.controller';
 import { BookingController } from './controllers/bookings/booking.controller';
 import { BookingService } from './services/bookings/booking.service';
+import { JwtAuthGuard } from './guards/jwt-auth.guard';
 
 const result = dotenv.config();
 
@@ -21,7 +20,7 @@ if (result.error) {
         TypeOrmModule.forFeature([Appointment, Booking]),
     ],
     controllers: [AppointmentController, BookingController],
-    providers: [AppointmentService, BookingService],
-    exports: [],
+    providers: [AppointmentService, BookingService, JwtAuthGuard],
+    exports: [JwtAuthGuard],
 })
 export class CommonModule { }
--- gym-back-end/src/common/guards/jwt-auth.guard.ts
+++ gym-back-end/src/common/guards/jwt-auth.guard.ts
@@ -1,11 +1,28 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { CanActivate, ExecutionContext, HttpException, HttpStatus, Injectable, Logger } from '@nestjs/common';
 import { Observable } from 'rxjs';
 
 @Injectable()
 export class JwtAuthGuard implements CanActivate {
+  private readonly logger = new Logger(JwtAuthGuard.name);
+
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
+    try {
+      this.logger.log('Estoy en el JWTAuthGuard');
+      // Obtenemos el objeto 'Request' de la ejecucion actual
+      const request = context.switchToHttp().getRequest();
+      // Intentamos recuperar el token de autenticacion desde las cookies
+      const token = request.cookies?.accessToken;
+      // Si no existe el token, denegamos el acceso
+      if (token === undefined) {
+        return false;
+      }
+    } catch (error) {
+      // Si ocurre algun error inesperado, lanzamos una excepcion 401 (No autorizado)
+      console.error("No hay token");
+      throw new HttpException("No existe token", HttpStatus.UNAUTHORIZED);
+    }
     return true;
   }
 }
