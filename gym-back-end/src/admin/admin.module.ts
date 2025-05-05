b4814f8f2a9f77ba55d496f2ef292f3476c7ab08
Franco_Serjen <francoserjen100@gmail.com>
2025-05-04 22:51:32 -0300

    Aplicación de guard para proteger las rutas de administrador

--- gym-back-end/src/admin/admin.module.ts
+++ gym-back-end/src/admin/admin.module.ts
@@ -1,4 +1,12 @@
 import { Module } from '@nestjs/common';
+import { JwtAuthGuard } from 'src/common/guards/jwt-auth.guard';
+import { AdminControllerController } from './controllers/admin-controller.controller';
+import { AdminService } from './services/admin.service';
 
-@Module({})
-export class AdminModule {}
+@Module({
+  imports: [],
+  controllers: [AdminControllerController],
+  providers: [JwtAuthGuard, AdminService],
+  exports: [],
+})
+export class AdminModule { }
