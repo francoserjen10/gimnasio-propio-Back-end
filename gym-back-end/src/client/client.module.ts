f41034781195d7effa1a6642cb45c5b734392f75
Franco_Serjen <francoserjen100@gmail.com>
2025-05-04 23:09:48 -0300

    Aplicación de guard para proteger las rutas de cliente

--- gym-back-end/src/client/client.module.ts
+++ gym-back-end/src/client/client.module.ts
@@ -1,4 +1,10 @@
 import { Module } from '@nestjs/common';
+import { ClientController } from './controllers/client.controller';
+import { JwtAuthGuard } from 'src/common/guards/jwt-auth.guard';
+import { ClientService } from './services/client.service';
 
-@Module({})
+@Module({
+  controllers: [ClientController],
+    providers: [JwtAuthGuard, ClientService],
+})
 export class ClientModule {}
