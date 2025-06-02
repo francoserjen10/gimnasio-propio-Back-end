c33fcb0ecde2e75672bb55f5af2f064e320b1743
Franco_Serjen <francoserjen100@gmail.com>
2025-06-02 20:54:06 -0300

    Aplición de guard de roles en admin

--- gym-back-end/src/admin/admin.module.ts
+++ gym-back-end/src/admin/admin.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { JwtAuthGuard } from 'src/common/guards/jwt-auth.guard';
 import { AdminService } from './services/admin.service';
 import { AdminController } from './controllers/admin.controller';
+import { RolesGuard } from 'src/common/guards/roles.guard';
 
 @Module({
   imports: [],
   controllers: [AdminController],
-  providers: [JwtAuthGuard, AdminService],
+  providers: [JwtAuthGuard, RolesGuard, AdminService],
   exports: [],
 })
 export class AdminModule { }
