2ac818e0ac6a21f23b6d853443a9b2edbdb290b5
Franco_Serjen <francoserjen100@gmail.com>
2025-06-10 12:15:16 -0300

    Modificación de nombre de controlador admin

--- gym-back-end/src/admin/admin.module.ts
+++ gym-back-end/src/admin/admin.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { JwtAuthGuard } from 'src/common/guards/jwt-auth.guard';
 import { AdminService } from './services/admin.service';
-import { AdminController } from './controllers/admin.controller';
 import { RolesGuard } from 'src/common/guards/roles.guard';
+import { UserController } from './controllers/user.controller';
 
 @Module({
   imports: [],
-  controllers: [AdminController],
+  controllers: [UserController],
   providers: [JwtAuthGuard, RolesGuard, AdminService],
   exports: [],
 })
--- gym-back-end/src/admin/controllers/user.controller.spec.ts
+++ gym-back-end/src/admin/controllers/user.controller.spec.ts
@@ -1,15 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { AdminController } from './admin.controller';
+import { UserController } from './user.controller';
 
-describe('AdminController', () => {
-  let controller: AdminController;
+describe('UserController', () => {
+  let controller: UserController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      controllers: [AdminController],
+      controllers: [UserController],
     }).compile();
 
-    controller = module.get<AdminController>(AdminController);
+    controller = module.get<UserController>(UserController);
   });
 
   it('should be defined', () => {
