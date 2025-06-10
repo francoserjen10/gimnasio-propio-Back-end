7edddd64ab378e071a27e030ba9df702f78f0895
Franco_Serjen <francoserjen100@gmail.com>
2025-06-10 14:07:18 -0300

    Controlador y servicio para la obtención de todos los usuarios listos

--- gym-back-end/src/admin/controllers/user.controller.ts
+++ gym-back-end/src/admin/controllers/user.controller.ts
@@ -1,9 +1,26 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Controller, Get, HttpException, HttpStatus, UseGuards } from '@nestjs/common';
 import { Roles } from 'src/common/decorators/roles.decorator';
 import { JwtAuthGuard } from 'src/common/guards/jwt-auth.guard';
 import { RolesGuard } from 'src/common/guards/roles.guard';
+import { UserService } from '../services/user.service';
 
 @Controller('admin')
 @UseGuards(JwtAuthGuard, RolesGuard)
 @Roles(1)
-export class UserController { }
+export class UserController {
+
+    constructor(private userService: UserService) { }
+
+    @Get()
+    async getAllUsers() {
+        try {
+            const response = await this.userService.getAllUsers();
+            return response;
+        } catch (error) {
+            if (error instanceof HttpException) {
+                throw error;
+            }
+            throw new HttpException({ message: 'Ocurrió un error para encontrar el listado de usuarios' }, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
--- gym-back-end/src/admin/services/user.service.ts
+++ gym-back-end/src/admin/services/user.service.ts
@@ -1,4 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/common/models/entities/user.entity';
+import { IUserResponse } from 'src/common/models/interfaces/user.interface';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+
+    constructor(@InjectRepository(User) private readonly userRepository: Repository<User>) { }
+
+    async getAllUsers() {
+        try {
+            const allUsers: IUserResponse[] = await this.userRepository.find();
+            return {
+                status: HttpStatus.OK,
+                message: 'Usuarios encontrados correctamente',
+                data: allUsers,
+            };
+        } catch (error) {
+            console.error("Error al traer el listado de usuarios:", error);
+            throw new HttpException({ message: 'Ocurrió un error para encontrar el listado de usuarios' }, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
